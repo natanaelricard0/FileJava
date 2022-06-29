54424d3c5e86f20757024a724605d0608a3fb201
Natanael Ricardo <nathanshurle@gmail.com>
2022-06-29 10:22:49 -0300

    lendo nome do arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,25 +9,14 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter a folder path: ");
+		System.out.println("Enter a file path: ");
 		String strPath = sc.nextLine();
 		
 		File path = new File(strPath);
 		
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS: ");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILERS");
-		for (File file : files) {
-			System.out.println(file);
-		}
-		
-		boolean sucess = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created sucessfuly: " + sucess);
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
 		
 		sc.close();
 
